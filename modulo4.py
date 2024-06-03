ea0fc35601f97d6129f090a19e60906183383580
MariaMendes11 <mcmendes900@gmail.com>
2024-06-03 15:59:01 -0300

    commit

---
+++ modulo4.py
@@ -0,0 +1,28 @@
+import time
+
+def somar():
+    n1 = int(input("Digite o primeiro numero da adição: "))
+    n2 = int(input("Digite o segundo numero da adição: "))
+    print(n1 + n2)
+
+# somar()
+
+#Função exclusiva de soma de dois numeros
+def somar2(n1 +n2):
+    soma = n1 + n2
+    return soma 
+
+# print("Soma 2:", somar2(22, 20))
+
+#Terceiro exemplo de função
+def somar3(n1 + n2):
+    return n1 + n2 
+numero1 = float(input("Digite um numero: "))
+numero2 = float(input("Digite outro numero: ")) 
+
+
+#Chamada  de função
+print(somar3(numero1, numero2))
+
+
+time.sleep(3)
